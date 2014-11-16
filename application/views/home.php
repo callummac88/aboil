dcf86e6f6ea6d50284442fd5e9037c54f0e16344
Callum Macdonald <callum@redevolution.com>
2014-11-16 15:24:23 +0000

    developed home view, added include to header, added bootstrap link
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

--- application/views/home.php
+++ application/views/home.php
@@ -3,19 +3,29 @@
 <head>
 	<meta charset="utf-8">
 	<title>Welcome</title>
+    <link rel="stylesheet" href="../public_html/assets/css/bootstrap.min.css ">
+
 
 </head>
 <body>
 
-<div>
-	<h1>Welcom To Aberdeen Oil Careers</h1>
+<?php include("header.html");?>
 
-	<div>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper metus metus. Mauris ut dui vel elit blandit fermentum sed non tortor. Ut at augue sed dolor sagittis consequat eu eu felis. Proin condimentum, ex quis porta suscipit, sem mauris porta neque, facilisis pulvinar nibh lectus eu metus. Vivamus dictum, sapien a fermentum consequat, velit lectus lobortis diam, vitae placerat nulla massa non quam. </p>
+<div class="container">
 
-	</div>
+    <div class="jumbotron">
+      <center>
+	<h2>Welcome To Aberdeen Oil Careers</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+         <button class="btn btn-primary">Read More</button>
+      </center>
+    </div>
+
+    <p>Aberdeen Oil Careers is the new home of oil job vacancy for the oil capital of the world</p>
+
 
 </div>
+
 
 </body>
 </html>
