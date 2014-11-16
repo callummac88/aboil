8e381c362dd3ef4cccb40a454836d21d2e2cac8e
Callum Macdonald <callum@redevolution.com>
2014-11-16 14:36:52 +0000

    create default home controller and view
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

---
+++ application/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome</title>
+
+</head>
+<body>
+
+<div>
+	<h1>Welcom To Aberdeen Oil Careers</h1>
+
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper metus metus. Mauris ut dui vel elit blandit fermentum sed non tortor. Ut at augue sed dolor sagittis consequat eu eu felis. Proin condimentum, ex quis porta suscipit, sem mauris porta neque, facilisis pulvinar nibh lectus eu metus. Vivamus dictum, sapien a fermentum consequat, velit lectus lobortis diam, vitae placerat nulla massa non quam. </p>
+
+	</div>
+
+</div>
+
+</body>
+</html>
